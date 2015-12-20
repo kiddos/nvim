dd1bd6525f6f956a77de495fda1dd7f775836ca4
kiddos <josephyu831007@yahoo.com.tw>
2015-12-20 17:36:56 +0800

    fix

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -125,7 +125,7 @@ flags = [
     '-I/usr/include/GNUstep/Renaissance',
     '-I/usr/include/GNUstep/TalkSoupBundles',
     # standard gcc includes
-    '-I/usr/lib/gcc/x86_64-linux-gnu/4.8/include'
+    '-I/usr/lib/gcc/x86_64-linux-gnu/4.8/include',
     # X11 lib
     '-I/usr/include/X11/',
     '-I.',
