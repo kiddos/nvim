05f1048e7dcb0aafacdea0a638b224c7f72c0f9b
kiddos <kiddo831007@gmail.com>
2023-11-12 09:55:22 +0800

    update server command

--- lua/server.lua
+++ lua/server.lua
@@ -1,24 +1,15 @@
 local M = {}
 
 M.start_apm_server = function()
-  vim.api.nvim_exec(
-  [[
-  silent exec ':!python3 ~/.config/nvim/scripts/apm_server.py --debug=False &'
-  ]], false)
+  vim.api.nvim_command('silent !python3 ~/.config/nvim/scripts/apm_server.py --debug=False &')
 end
 
 M.start_codellama_code_server = function()
-  vim.api.nvim_exec(
-  [[
-  silent exec ':!python3 ~/.config/nvim/scripts/ml_server.py --model ~/models/ggml/codellama/codellama-7b.Q2_K.gguf --port 60001 --prompt_type=code 1> /dev/null &'
-  ]], false)
+  vim.api.nvim_command('silent !python3 ~/.config/nvim/scripts/ml_server.py --model ~/models/ggml/codellama/codellama-7b.Q2_K.gguf --port 60001 --prompt_type=code 1> /dev/null &')
 end
 
 M.start_codellama_instruct_server = function()
-  vim.api.nvim_exec(
-  [[
-  silent exec ':!python3 ~/.config/nvim/scripts/ml_server.py --model ~/models/ggml/codellama/codellama-7b-instruct.Q2_K.gguf --port 60002 --prompt_type=instruct 1> /dev/null &'
-  ]], false)
+  vim.api.nvim_command('silent !python3 ~/.config/nvim/scripts/ml_server.py --model ~/models/ggml/codellama/codellama-7b-instruct.Q2_K.gguf --port 60002 --prompt_type=instruct 1> /dev/null &')
 end
 
 return M
