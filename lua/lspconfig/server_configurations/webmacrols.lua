9fd0f54ecff91f2fbdba38aff7d65f2c5496b832
kiddos <kiddo831007@gmail.com>
2023-12-13 01:28:23 +0800

    format

--- lua/lspconfig/server_configurations/webmacrols.lua
+++ lua/lspconfig/server_configurations/webmacrols.lua
@@ -1,11 +1,12 @@
 local util = require('lspconfig/util')
+local uv = vim.uv or vim.loop
 
 return {
   default_config = {
-    cmd = {"webmacro-language-server", "--stdio"},
-    filetypes = {"webmacro"},
+    cmd = { "webmacro-language-server", "--stdio" },
+    filetypes = { "webmacro" },
     root_dir = function(fname)
-      return util.root_pattern('build.xml', '.git', 'ivy.xml')(fname) or vim.uv.os_homedir()
+      return util.root_pattern('build.xml', '.git', 'ivy.xml')(fname) or uv.os_homedir()
     end
   }
 }
