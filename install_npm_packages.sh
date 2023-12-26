66e724f9079467780a3b7dc875f4afe3ce9df0d7
kiddos <kiddo831007@gmail.com>
2023-12-27 01:15:00 +0800

    update requirements

---
+++ install_npm_packages.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+# lsp
+npm install -g bash-language-server typescript-language-server typescript vscode-langservers-extracted cssmodules-language-server
+# tools
+npm install -g prettier
