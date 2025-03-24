a028e44f9872457ece399a76b47f9c0d35309542
kiddos <kiddo831007@gmail.com>
2025-03-24 22:51:43 +0800

    emmet for jsp

--- lua/plugins/util/emmet.lua
+++ lua/plugins/util/emmet.lua
@@ -3,5 +3,10 @@ return {
   ft = { 'html', 'javascript', 'jsp' },
   config = function()
     vim.api.nvim_set_var('user_emmet_togglecomment_key', '<C-y>#')
+    vim.api.nvim_set_var('user_emmet_settings', {
+      jsp = {
+        indentation = '  ',
+      }
+    })
   end,
 }
