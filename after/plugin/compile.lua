86939891b9116eada193391305b671018a64f18c
kiddos <kiddo831007@gmail.com>
2025-07-09 23:57:20 +0800

    fix Ctrl f9

--- after/plugin/compile.lua
+++ after/plugin/compile.lua
@@ -1,22 +1,24 @@
+local api = vim.api
+
 local register_command = function(filetype, key, command)
-  vim.api.nvim_create_autocmd('FileType', {
+  api.nvim_create_autocmd('FileType', {
     pattern = filetype,
     callback = function()
-      local current = vim.api.nvim_get_current_buf()
+      local current = api.nvim_get_current_buf()
 
-      vim.api.nvim_buf_create_user_command(current, 'Compile', command, {
+      api.nvim_buf_create_user_command(current, 'Compile', command, {
         force = true,
         desc = 'compile code',
       })
 
       local modes = { 'n', 'i' }
       for _, mode in pairs(modes) do
-        vim.api.nvim_buf_set_keymap(current, mode, key, '', {
+        api.nvim_buf_set_keymap(current, mode, key, '', {
           silent = true,
           noremap = true,
           desc = 'compile code',
           callback = function()
-            vim.api.nvim_command(command)
+            api.nvim_command(command)
           end
         })
       end
@@ -24,7 +26,7 @@ local register_command = function(filetype, key, command)
   })
 end
 
-local key = '<C-F9>'
+local key = '<F33>'
 
 register_command({ 'c', 'cpp' }, key, '!clang++ % -Wall -Wextra -Wshadow -std=c++20 -fsanitize=address -O1 -g -o %:r')
 register_command({ 'rust' }, key, '!rustc %')
