a714c9c966ddfe2aa9c59d650b28963678322ca2
kiddos <kiddo831007@gmail.com>
2025-07-10 00:33:50 +0800

    jsp comment

--- lua/plugins/util/comment.lua
+++ lua/plugins/util/comment.lua
@@ -8,17 +8,16 @@ local config = function()
 
   local ft = require('Comment.ft')
   ft.webmacro = '## %s'
+  ft.jsp = '<%-- %s --%>'
 
   vim.api.nvim_set_keymap('n', '<leader>cc', '<Plug>(comment_toggle_linewise_current)', {})
   vim.api.nvim_set_keymap('n', '<leader>cu', '<Plug>(comment_toggle_linewise_current)', {})
   vim.api.nvim_set_keymap('v', '<leader>cc', '<Plug>(comment_toggle_linewise_visual)', {})
   vim.api.nvim_set_keymap('v', '<leader>cu', '<Plug>(comment_toggle_linewise_visual)', {})
-  vim.api.nvim_set_keymap('n', '<C-_>', '<Plug>(comment_toggle_linewise_current)', {})
-  vim.api.nvim_set_keymap('v', '<C-_>', '<Plug>(comment_toggle_linewise_visual)', {})
 end
 
 return {
   'numToStr/Comment.nvim',
-  event = 'VimEnter',
+  keys = {'<leader>cc', '<leader>cu' },
   config = config,
 }
