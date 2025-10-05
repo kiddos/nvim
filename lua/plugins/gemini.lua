cfbb9093606d881c662dbc6431e1a57f4dc40975
kiddos <kiddo831007@gmail.com>
2025-10-05 15:31:04 +0800

    update gemini setting

--- lua/plugins/gemini.lua
+++ lua/plugins/gemini.lua
@@ -4,11 +4,18 @@ return {
     'kiddos/pawtocomplete.nvim',
   },
   event = 'InsertEnter',
+  cmd = { 'GeminiTask', 'GeminiChat' },
   opts = {
     completion = {
       can_complete = function()
         return not require('pawtocomplete.completion_menu').is_opened()
       end
+    },
+    chat_config = {
+      window = {
+        position = "right",     -- left, right, new_tab, tab
+        width = 80,               -- number of columns of the left/right window
+      }
     }
   }
 }
