d1c5c1b2c6d66c5e6eb32fbe3e229db2c93bb498
kiddos <kiddo831007@gmail.com>
2025-10-11 15:06:23 +0800

    remove build

--- lua/plugins/google-translate.lua
+++ lua/plugins/google-translate.lua
@@ -1,6 +1,5 @@
 return {
   'kiddos/google-translate.nvim',
-  build = { 'pip install -r requirements.txt', ':UpdateRemotePlugins' },
   cmd = { 'TranslateCN', 'TranslateTW', 'TranslateEN' },
   opts = {},
 }
